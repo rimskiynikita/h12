30ef109ad3bc039d29e74fa1d42cfe0d8fbe0bdc
Nikita Rimskiy <rimskiy.nikita@gmail.com>
2016-11-10 02:16:29 +0300

    hm2B

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import express from 'express';
 import cors from 'cors';
 
+function shortenName(fullname) {
+  const fullnameArray = fullname.split(' ')
+  console.log(fullname.replace(/ /g,''))
+  const regex = /^[a-zа-яÆÐƎƏƐƔĲŊŒẞÞǷȜæðǝəɛɣĳŋœĸſßþƿȝĄƁÇĐƊĘĦĮƘŁØƠŞȘŢȚŦŲƯY̨Ƴąɓçđɗęħįƙłøơşșţțŧųưy̨ƴÁÀÂÄǍĂĀÃÅǺĄÆǼǢƁĆĊĈČÇĎḌĐƊÐÉÈĖÊËĚĔĒĘẸƎƏƐĠĜǦĞĢƔáàâäǎăāãåǻąæǽǣɓćċĉčçďḍđɗðéèėêëěĕēęẹǝəɛġĝǧğģɣĤḤĦIÍÌİÎÏǏĬĪĨĮỊĲĴĶƘĹĻŁĽĿʼNŃN̈ŇÑŅŊÓÒÔÖǑŎŌÕŐỌØǾƠŒĥḥħıíìiîïǐĭīĩįịĳĵķƙĸĺļłľŀŉńn̈ňñņŋóòôöǒŏōõőọøǿơœŔŘŖŚŜŠŞȘṢẞŤŢṬŦÞÚÙÛÜǓŬŪŨŰŮŲỤƯẂẀŴẄǷÝỲŶŸȲỸƳŹŻŽẒŕřŗſśŝšşșṣßťţṭŧþúùûüǔŭūũűůųụưẃẁŵẅƿýỳŷÿȳỹƴźżžẓ]+$/i
+  var shortenFullName = ''
+  if (fullnameArray.length <= 3 && fullnameArray != '' && regex.test(fullname.replace(/ /g,''))) {
+    shortenFullName += fullnameArray[fullnameArray.length - 1]
+    if (fullnameArray.length != 1) {
+      for (var i = 0; i < fullnameArray.length-1; i++) {
+        shortenFullName += ' ' + fullnameArray[i][0] + '.';
+      }
+    }
+  } else {
+    shortenFullName = 'Invalid fullname'
+  }
+  return shortenFullName
+}
+
 const app = express();
 app.use(cors());
-app.get('/', (req, res) => {
-  res.json({
-    hello: 'JS World',
-  });
-});
 
-app.get('/task2A', (req, res) => {
-  const sum = (+req.query.a || 0) + (+req.query.b || 0)
-  res.send(sum.toString())
+app.get('/task2B', (req, res) => {
+  const fullname = req.query.fullname
+  const shortenFullname = shortenName(fullname)
+  res.send(shortenFullname)
 });
 
 app.listen(3000, () => {
-  console.log('Your app listening on port 3000!');
+  console.log('Your app listening on port 3000!')
 });
